20df58e6c9ca27e8b33855be8f888270010a60cd
Sebastian Tellez <sebastian.tellez.o@gmail.com>
2018-03-04 09:19:26 -0600

    add verify ID photo

--- app/src/main/java/com/chilangolabs/hackcicm/activitys/RegisterFormActivity.kt
+++ app/src/main/java/com/chilangolabs/hackcicm/activitys/RegisterFormActivity.kt
@@ -3,11 +3,17 @@ package com.chilangolabs.hackcicm.activitys
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.chilangolabs.hackcicm.R
+import kotlinx.android.synthetic.main.activity_register_form.*
+import org.jetbrains.anko.startActivity
 
 class RegisterFormActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_form)
+
+        btnRegister?.setOnClickListener {
+            startActivity<VerifyIDActivity>()
+        }
     }
 }
